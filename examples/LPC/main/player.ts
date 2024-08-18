e8a55b300ab4cb906215c029c5f9b069108946a1
lyzz <lyzz@karo4.de>
2024-08-18 17:29:21 +0200

    Cleaned up

--- examples/LPC/main/player.ts
+++ examples/LPC/main/player.ts
@@ -1,25 +1,49 @@
 import { RpgPlayer, RpgPlayerHooks, Control, Components } from '@rpgjs/server'
 
-const graphics = ['pale-green-body', 'pale-green-head', 'pale-green-wings', 'pale-green-wings-fg', 'dark-grey-coat', 'boots-black', 'hood-black'];
+const naked = ['lavender-fem-body', 'lavender-fem-head',  'lavender-elven-ears', 'blue-eyes', 'lavender-nose-straight', 'purple-thin-eyebrows', 'purple-bangs-fem'];
+
+const clothes = [
+{name:'naked', clothes:[]},
+{name: 'default', clothes:['teal-kimono-fem']},
+{name: 'everyday', clothes:['red-skirt-fem','red-vneckshirt-fem']},
+{name: 'colorclash', clothes: ['red-skirt-fem', 'green-vneckshirt-fem']},
+{name: 'glamourdress', clothes: ['red-tie-slit-fem']},
+{name: 'sexyblack', clothes:['black-bodice-fem']}]
+
+const extras = ['bowtie2-black'];
+let outfitNumber = 1;
+
+
+function changeOutfit(outfitNumber: number) {
+     }
 
 const player: RpgPlayerHooks = {
-    onConnected(player: RpgPlayer) {
-        player.name = 'Dragon'
+   
+     
+   onConnected(player: RpgPlayer) {
+        player.name = 'Kimi'
         player.setComponentsTop(Components.text('{name}'))
-
-        player.setGraphic(graphics);
+	player.setGraphic(naked.concat(clothes[outfitNumber].clothes.flat()).concat(extras.flat()));
+	
+        //player.setGraphic(graphics);
     },
     onInput(player: RpgPlayer, { input }) {
         if (input == Control.Back) {
-            player.callMainMenu()
+	   player.callMainMenu()
         }
 
         if (input === Control.Attack) {
-            player.showAnimation(graphics, 'attack', true);
+	 outfitNumber = outfitNumber === 0 ? 0 : outfitNumber -1;
+	const outfit = naked.concat(clothes[outfitNumber].clothes.flat()).concat(extras.flat());
+	   player.setGraphic(outfit);
+	   player.showAnimation(outfit, 'skill', true);	
         }
 
         if (input === Control.Skill) {
-            player.showAnimation(graphics, 'skill', true);
+	   outfitNumber = outfitNumber === clothes.length -1 ? clothes.length-1 : outfitNumber +1;
+	   const outfit = naked.concat(clothes[outfitNumber].clothes.flat()).concat(extras.flat());
+	   player.setGraphic(outfit);
+	   player.showAnimation(outfit, 'skill', true);	
         }
     },
     async onJoinMap(player: RpgPlayer) {
